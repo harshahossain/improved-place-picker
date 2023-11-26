9d32fe52f8b4e5d1ba10bbec99e5bf0312d9a3ff
harshahossain <harshahossain@gmail.com>
2023-11-26 12:20:59 +0600

    oads Images for available places
    fetched data from backend to show users the available places

--- backend/app.js
+++ backend/app.js
@@ -1,53 +1,55 @@
-import fs from 'node:fs/promises';
+import fs from "node:fs/promises";
 
-import bodyParser from 'body-parser';
-import express from 'express';
+import bodyParser from "body-parser";
+import express from "express";
 
 const app = express();
 
-app.use(express.static('images'));
+app.use(express.static("images")); //exposing all the files inside 'images' folder directly on the root backend server
+//check the front end Places and check image source using string template literal
+
 app.use(bodyParser.json());
 
 // CORS
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   next();
 });
 
-app.get('/places', async (req, res) => {
-  const fileContent = await fs.readFile('./data/places.json');
+app.get("/places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/places.json");
 
   const placesData = JSON.parse(fileContent);
 
   res.status(200).json({ places: placesData });
 });
 
-app.get('/user-places', async (req, res) => {
-  const fileContent = await fs.readFile('./data/user-places.json');
+app.get("/user-places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/user-places.json");
 
   const places = JSON.parse(fileContent);
 
   res.status(200).json({ places });
 });
 
-app.put('/user-places', async (req, res) => {
+app.put("/user-places", async (req, res) => {
   const places = req.body.places;
 
-  await fs.writeFile('./data/user-places.json', JSON.stringify(places));
+  await fs.writeFile("./data/user-places.json", JSON.stringify(places));
 
-  res.status(200).json({ message: 'User places updated!' });
+  res.status(200).json({ message: "User places updated!" });
 });
 
 // 404
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
-  res.status(404).json({ message: '404 - Not Found' });
+  res.status(404).json({ message: "404 - Not Found" });
 });
 
-app.listen(3000);
+app.listen(3000, console.log("listining at 3k"));
